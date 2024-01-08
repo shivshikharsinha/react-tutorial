d8a8bf2075ab444cd606f648a8b5ed570c6f4d9c
Shiv Shikhar Sinha <shiv.shikhar.3@gmail.com>
2024-01-09 02:30:46 +0530

    edited custom react js file

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,3 +1,10 @@
-const reactElement = 
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://www.google.com',
+        target: '_blank'
+    },
+    children: 'Click Me to visit Google'
+}
 
 const mainContainer = document.getElementById('root')
