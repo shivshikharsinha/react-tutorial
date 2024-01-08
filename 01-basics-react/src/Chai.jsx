d3156bfe53941ecb9388fa34f415703b6015d369
Shiv Shikhar Sinha <shiv.shikhar.7@gmail.com>
2024-01-09 01:49:42 +0530

    Understood flow of react and structure

---
+++ 01-basics-react/src/Chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h2> Shiv Loves Chai</h2>
+    )
+}
+export default Chai;
---
+++ 01-vite-project/src/Chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h2>Shiv Loves Vite Chai</h2>
+    )
+}
+export default Chai;
