d15e00d60afb743b5bb0372c17d5482b7115ebe8
Shiv Shikhar Sinha <shiv.shikhar.3@gmail.com>
2024-01-09 22:54:44 +0530

    Custom React Actually Done

--- 01-vite-project/src/App.jsx
+++ 01-vite-project/src/App.jsx
@@ -1,10 +1,10 @@
 
 import Chai from "./Chai"
 function App() {
-
+const username = "shivshikharsinha";
   return (
     <>
-    <h1> Element from App Itself</h1>
+    <h1> Element from App Itself {username}</h1>
     <Chai/>
     </>
   )
--- 01-vite-project/src/main.jsx
+++ 01-vite-project/src/main.jsx
@@ -2,7 +2,29 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// function MyApp(){
+//   return (
+//     <div>
+//       <h2>
+//       Custom App
+//       </h2>
+//     </div>
+//   )
+// }
+// const anotherElement = (
+//   <a href = 'https://shivshikharsinha.netlify.app' target='_blank' rel="noreferrer">Visit Google</a>
+// )
+
+// const anotherUser = "Sahil"
+
+// const reactElement = React.createElement(
+//   'a',
+//   {href: "http://www.facebook.com", target: '_blank'},
+//   "Click me to visit facebook",
+//   anotherUser
+// )
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <App/>
 )
