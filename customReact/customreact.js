21efde48a215a5527686c064426e4228ea921506
Shiv Shikhar Sinha <shiv.shikhar.3@gmail.com>
2024-01-09 21:42:00 +0530

    custom React done

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,3 +1,25 @@
+function customRender(reactElement, container){
+    
+    /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+    
+    container.appendChild(domElement);
+    */
+
+
+   const domElement = document.createElement(reactElement.type)
+   domElement.innerHTML = reactElement.children
+
+   for (const prop in reactElement.props) {
+    if (prop === 'children') continue;
+    domElement.setAttribute(prop, reactElement.props[prop])
+   }
+
+   container.appendChild(domElement)
+}
 const reactElement = {
     type: 'a',
     props: {
@@ -8,3 +30,4 @@ const reactElement = {
 }
 
 const mainContainer = document.getElementById('root')
+customRender(reactElement, mainContainer)
