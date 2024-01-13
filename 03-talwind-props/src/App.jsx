0b2d5a6a612f852266aace76b7a1ef7d387d66a7
Shiv Shikhar Sinha <shiv.shikhar.3@gmail.com>
2024-01-13 16:52:16 +0530

    React Foundation Completed

--- 03-talwind-props/src/App.jsx
+++ 03-talwind-props/src/App.jsx
@@ -5,13 +5,16 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-
+  // let myObj = {
+  //   username: "shiv",
+  //   age: 23
+  // }
+  // let newArr =[1,2,3,4,5] 
   return (
     <>
-    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4' >Tailwind Test</h1>
-    
-<Card channel = "Chai Aur Code"/>
-<Card/>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4' >Tailwind Test</h1>
+      <Card  btnText = "Click Me"/>
+      <Card username = "ShivshikharSinha" />
     </>
   )
 }
