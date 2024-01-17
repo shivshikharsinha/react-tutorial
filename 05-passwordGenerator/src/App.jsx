02bd9fea786b6f33dd6550ac75066ff110b8450d
Shiv Shikhar Sinha <shiv.shikhar.3@gmail.com>
2024-01-17 23:37:35 +0530

    added changed in App.jsx

--- 05-passwordGenerator/src/App.jsx
+++ 05-passwordGenerator/src/App.jsx
@@ -13,13 +13,26 @@ function App() {
     let pass = ""
     let str ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-    if(numberAllowed) str+= "0123456789"
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()_+-=[]{}`~"
+
+    for (let i = 1; i<=length; i++) {
+      let char = Math.floor(Math.random()*str.length +1)
+      pass = str.charAt(char);
+      setPassword(pass);
+    }
+
   }, [length, numberAllowed, charAllowed, setPassword])
 
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500">
+      <h1 className='text-4xl mb-4 mt-4 text-center text-white'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input type='text' value={password} className='outline-none mb-4 rounded w-full py-1 px-3' placeholder='password' readOnly></input>
+        </div>
+      </div>
     </>
   )
 }
