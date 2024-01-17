f72689c194b0a7b0b0d0681e52bdd0431c60d1da
Shiv Shikhar Sinha <shiv.shikhar.3@gmail.com>
2024-01-18 00:09:05 +0530

    Just before useEffect done

--- 05-passwordGenerator/src/App.jsx
+++ 05-passwordGenerator/src/App.jsx
@@ -24,15 +24,61 @@ function App() {
 
   }, [length, numberAllowed, charAllowed, setPassword])
 
-
+  
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-500">
-      <h1 className='text-4xl mb-4 mt-4 text-center text-white'>Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+      <h1 className='text-2xl mb-4 mt-4 text-center text-white'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-          <input type='text' value={password} className='outline-none mb-4 rounded w-full py-1 px-3' placeholder='password' readOnly></input>
+
+        <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            // ref={passwordRef}
+        />
+
+          <button className='outline-none bg-blue-500 text-white px-3 py-0.5 shrink-0' 
+          onClick={() => passwordGenerator()}>Copy</button>
         </div>
+        <div className='flex text-sm gap-x-2'>
+      <div className='flex mb-4 items-center gap-x-1'>
+        <input 
+        type="range"
+        min={6}
+        max={32}
+        value={length}
+         className='cursor-pointer'
+         onChange={(e)=> {setlength(e.target.value)}}
+         />
+          <label>Length: {length}</label>
       </div>
+      <div className="flex mb-4 items-center gap-x-1">
+      <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={() => {
+              setNumberAllowed((prev) => !prev);
+          }}
+      />
+      <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className="flex mb-4 items-center gap-x-1">
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                  setCharAllowed((prev) => !prev )
+              }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+      </div>
+    </div>
+</div>
     </>
   )
 }
